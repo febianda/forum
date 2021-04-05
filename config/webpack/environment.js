07e94fcf019bb1ab8c998a353dce09ec2b2bffb0
Febianda <febianda82@gmail.com>
2021-04-05 13:56:57 +0700

    Bismillah semoga jago Ruby on Rails

---
+++ config/webpack/environment.js
@@ -0,0 +1,11 @@
+const {  } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
+module.exports = environment
